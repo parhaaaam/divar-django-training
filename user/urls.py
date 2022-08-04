5edf6d4afc60bc1436f2c55d7ca1818209f4e18f
Parham <parhamhohoho@gmail.com>
2022-08-04 15:43:44 +0430

    applying part 4 and 8

---
+++ user/urls.py
@@ -0,0 +1,12 @@
+
+from . import views 
+from django.urls import path 
+
+urlpatterns = [
+
+    path('' , views.Index.as_view() , name = 'index') ,
+    path('/login', views.Log_in.as_view(), name="login"),
+    path('/register' , views.Register.as_view() , name = 'register') , 
+    path('/profile' , views.Profile.as_view() , name = 'profile') ,
+    path('/profile/update' , views.Update.as_view() , name = 'update')
+]
--- user/views.py
+++ user/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.forms import AuthenticationForm 
 from django.contrib.auth import authenticate  , login 
+from django.views.generic.edit import FormView, CreateView , UpdateView
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from . import forms
+
 
 
 class Index (TemplateView) : 
@@ -13,13 +19,13 @@ class Log_in(TemplateView) :
     template_name = 'login.html'
     error = None
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.error:
             context['error'] = self.error
         return context
 
-    def post (request) : 
+    def post (self, request) : 
 
         username = request.POST['username']
         password = request.POST['password'] 
@@ -29,10 +35,32 @@ class Log_in(TemplateView) :
 
             login(request , current_user) 
         
+            return redirect("/user")
         else :
-            return self.get()
+            self.error = "Invalid Username/password"
+            return self.get(request)
 
 
-class register() : 
+class Register(CreateView) : 
+    form_class = forms.NewUserForm
+    template_name = 'register.html'
+    success_url = '/user'
 
-    pass
+    def form_valid(self, form):
+        resp = super().form_valid(form)
+        user = self.object
+        print(user)
+        user = authenticate(self.request, username=user.username, password=form.cleaned_data.get('password1'))
+        login(self.request, user)
+        return resp
+    
+    
+class Profile(TemplateView) : 
+
+    template_name = 'profile.html'
+
+class Update(UpdateView) : 
+
+    model = User
+    fileds = ['first_name ' , 'last_name']
+    templlate_name = 'update.html'
